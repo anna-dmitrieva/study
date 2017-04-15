19053c0d897ecf1bbdbdc5492c7107ac9f5b7182
anna-dmitrieva <root@68994.local>
2017-04-15 21:44:41 +0000

    add new

--- 1script.sh
+++ 1script.sh
@@ -2,6 +2,11 @@
 #
 # 
 rm -rf ~/testtask/
-mkdir ~/testtask/
-touch ~/testtask/testfile{1..10}
-wget admitriva.com/dhclient.conf
+> ~/testlog
+touch ~/testlog | echo 'create log file' >> ~/testlog
+mkdir ~/testtask/ | echo 'create dir testtask' >> ~/testlog
+touch ~/testtask/testfile{1..10} | echo 'create files testfile[1:10]in directory testtask' >> ~/testlog
+wget admitrieva.com/dhclient.conf | echo 'download config from internet' >> ~/testlog
+cat dhclient.conf >  ~/testtask/testfile{1..10} | echo 'copy config file to testfile[1:10]' >> ~/testlog
+
+
