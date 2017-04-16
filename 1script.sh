b939e636333742b512e9599e63013e19043557b6
root <root@68994.local>
2017-04-16 00:20:24 +0000

    modified script

--- 1script.sh
+++ 1script.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #
 # 
-rm -rf ~/testtask/
-> ~/testlog
+rm -rf ~/testtask/ | > ~/testlog
 touch ~/testlog | echo 'create log file' >> ~/testlog
 chmod 444 ~/testlog | echo 'modificate logfile for read any user' >> ~/testlog
 mkdir ~/testtask/ | echo 'create dir testtask' >> ~/testlog
 touch ~/testtask/testfile{1..10} | echo 'create files testfile[1:10]in directory testtask' >> ~/testlog
-wget admitrieva.com/dhclient.conf -O ~/testtask/dhclient.conf | echo 'download config from internet' >> ~/testlog
-for i in `ls -1 ~/testtask/ | grep -v dhclient`; do cat ~/testtask/dhclient.conf > ~/testtask/$i; done  | echo 'copy config file to testfile[1:10]in directory testtask' >> ~/testlog
+echo '--- start download config file' >> ~/testlog | touch ~/wgetlog
+wget -b admitrieva.com/dhclient.conf -O ~/testtask/dhclient.conf -a ~/testlog
+echo '--- end download config from internet' >> ~/testlog
+for i in `ls -1 ~/testtask/ | grep -v dhclient`; do cat ~/testtask/dhclient.conf > ~/testtask/$i; done 
+echo 'copy config file to testfile[1:10]in directory testtask' >> ~/testlog
 echo 'list files:' >> ~/testlog
 ls -1 ~/testtask/*  >> ~/testlog | ls ~/testlog >> ~/testlog
-#for i in `ls -1 ~/testtask/ | grep -v dhclient`; do mv -v $i /* >> ~/testlog ; done | echo 'end move fucking file from / to some depth filesystem' >> ~/testlog
+echo '--- start move files fron testtask to /*'
+for i in `ls -1 ~/testtask/ | grep -v dhclient`; do for x in `find / -maxdepth 1 -type d|grep -v proc | grep -v sys`; do cp -v ~/testtask/$i $x/$I >> ~/testlog ; done ; done; rm ~/testtask/t*; 
+echo '--- end move fucking file from / to some depth filesystem' >> ~/testlog
 
