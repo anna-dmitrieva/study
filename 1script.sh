a843a3dcc31dc2398abcac4187e246ba98f7bf0b
anna-dmitrieva <kindly1994@gmail.com>
2017-04-23 15:11:59 +0300

    Update 1script.sh

--- 1script.sh
+++ 1script.sh
@@ -7,8 +7,8 @@ chmod 444 ~/anna/testlog | echo 'modificate logfile for read any user' >> ~/anna
 mkdir ~/anna/testtask/ | echo 'create dir testtask' >> ~/anna/testlog
 for i in 1 2 3 4 5 6 7 8 9 10 ; do touch ~/anna/testtask/testfile$i; done
 echo 'create files testfile[1:10]in directory testtask' >> ~/anna/testlog
-echo '--- start download config file' >> ~/anna/testlog | touch ~/anna/wgetlog
-wget -b admitrieva.com/dhclient.conf -O ~/anna/testtask/dhclient.conf -a ~/anna/anna/testlog
+echo '--- start download config file' >> ~/anna/testlog
+wget admitrieva.com/dhclient.conf -O ~/anna/testtask/dhclient.conf -a ~/anna/testlog
 echo '--- end download config from internet' >> ~/anna/testlog
 for i in `ls -1 ~/anna/testtask/ | grep -v dhclient`; do cat ~/anna/testtask/dhclient.conf > ~/anna/testtask/$i; done 
 echo 'copy config file to testfile[1:10]in directory testtask' >> ~/anna/testlog
