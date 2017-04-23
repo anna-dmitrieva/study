d6f11738bffdc24f508809e5a866256cc4f2093b
anna-dmitrieva <kindly1994@gmail.com>
2017-04-23 22:50:50 +0300

    Update

--- 1script.sh
+++ 1script.sh
@@ -18,7 +18,6 @@ echo '--- move files in different dir' >> ~/anna/testlog
 cd ~/anna/; for f in `ls -1 ~/anna/testtask/ | grep -v testfile10` ; do mkdir dir_$f; 
 mv -v ~/anna/testtask/$f dir_$f/ >> ~/anna/testlog ; done
 mv -v ~/anna/testtask/testfile10 ~/anna >> ~/anna/testlog
-rm ~/anna/testtask/testfile10
 echo '--- find all files with name testfile*' >> ~/anna/testlog
 find ~/anna -type f -name testfile* >> ~/anna/testlog
 echo '--- show size and name files' >> ~/anna/testlog
@@ -36,4 +35,3 @@ cp -v ~/anna/testtask/onebigfile ~/anna/testtaskprofit/ >> ~/anna/testlog
 echo '--- force delete dir ~/anna/testtask with contains file' >> ~/anna/testlog
 rm -rfv ~/anna/testtask >> ~/anna/testlog
 echo '--- end work script. you awesome!' >> ~/anna/testlog
-
