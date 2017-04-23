a8859ea8808e36e88114943937c65641d4bde861
anna-dmitrieva <kindly1994@gmail.com>
2017-04-24 00:49:23 +0300

    Update

--- 2script.sh
+++ 2script.sh
@@ -3,4 +3,5 @@
 mkdir ~/anna
 touch ~/anna/script.log
 echo $(date -u)": Started" >> ~/anna/script.log
-for n in $@ ;do if n==4; then echo "$n"; else echo 'You nead to provide 4 parameters'; done ; done
+n=$@
+if n==4; then echo "$n"; else echo 'You nead to provide 4 parameters'; fi
