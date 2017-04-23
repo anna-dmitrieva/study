dc0e36bb9514ea87d81ae5ea2a79c52f3dfbc59a
anna-dmitrieva <kindly1994@gmail.com>
2017-04-23 16:40:30 +0300

    Update 1script.sh

--- 1script.sh
+++ 1script.sh
@@ -8,10 +8,12 @@ mkdir ~/anna/testtask/ | echo 'create dir testtask' >> ~/anna/testlog
 for i in 1 2 3 4 5 6 7 8 9 10 ; do touch ~/anna/testtask/testfile$i; done
 echo 'create files testfile[1:10]in directory testtask' >> ~/anna/testlog
 echo '--- start download config file' >> ~/anna/testlog
-wget admitrieva.com/dhclient.conf -O ~/anna/testtask/dhclient.conf -a ~/anna/testlog
+wget admitrieva.com/dhclient.conf -O ~/anna/dhclient.conf -a ~/anna/testlog
 echo '--- end download config from internet' >> ~/anna/testlog
-for i in `ls -1 ~/anna/testtask/ | grep -v dhclient`; do cat ~/anna/testtask/dhclient.conf > ~/anna/testtask/$i; done 
+for i in `ls -1 ~/anna/testtask/ | grep -v dhclient`; do cat ~/anna/dhclient.conf > ~/anna/testtask/$i; done 
 echo 'copy config file to testfile[1:10]in directory testtask' >> ~/anna/testlog
 echo 'list files:' >> ~/anna/testlog
 ls -1h ~/anna/testtask/*  >> ~/anna/testlog | ls ~/anna/testlog >> ~/anna/testlog
-for l in 1 2 3 4 5 6 7 8 9 ; do mkdir ~/anna/$l ; done
+cd ~/anna/
+for f in `ls -1 ~/anna/testtask/ | grep -v testfile10` ; do mkdir dir_$f 
+mv ~/anna/testtask/$f dir_$f/ ; done
