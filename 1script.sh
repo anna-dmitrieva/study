46797249347f0626ca72f9f9b2d4ed675aa63179
anna-dmitrieva <kindly1994@gmail.com>
2017-04-23 22:36:10 +0300

    Update 1script.sh

--- 1script.sh
+++ 1script.sh
@@ -25,7 +25,7 @@ find ~/anna -type f -name testfile* | xargs ls -lh | awk '{print $5 " " $9 }' >>
 echo '--- mv files to ~/anna/testtask/' >> ~/anna/testlog
 find ~/anna -type f -name testfile* | xargs mv -v -t ~/anna/testtask/ '{}';
 echo '--- concatination all file in one big file' >> ~/anna/testlog
-find ~/anna -type f -name testfile* | xargs cat '{}' > ~/anna/testtask/onebigfile 
+find ~/anna -type f -name testfile* | xargs cat > ~/anna/testtask/onebigfile 
 echo '--- delete all unused file' >> ~/anna/testlog
 find ~/anna -type f -name testfile* | xargs rm
 echo '--- create dir testtaskprofit' >> ~/anna/testlog
