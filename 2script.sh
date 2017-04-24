c8787ea498508b4e481c9ba3a1b3ad43b03998b0
anna-dmitrieva <kindly1994@gmail.com>
2017-04-24 15:54:40 +0300

    Update 2script.sh

--- 2script.sh
+++ 2script.sh
@@ -2,4 +2,4 @@
 #
 touch ~/anna/script.log
 echo $(date -u)": Started" >> ~/anna/script.log
-if [ "$#" -eq "4" ]; then echo "$1/n$2/n$3/n$4"; else echo 'You nead to provide 4 parameters'; fi
+if [ "$#" -eq "4" ]; then echo -e "$1/n$2/n$3/n$4"; else echo 'You nead to provide 4 parameters'; fi
