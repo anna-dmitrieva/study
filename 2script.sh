956bf21be5280bae69db709c30e76af14934326f
anna-dmitrieva <kindly1994@gmail.com>
2017-04-24 20:07:32 +0300

    Update 2script.sh

--- 2script.sh
+++ 2script.sh
@@ -2,5 +2,9 @@
 #
 touch ~/anna/script.log
 echo $(date -u)": Started" >> ~/anna/script.log
-if [ "$#" -eq "4" ] ; then if [[ $1 == ftp://* || $1 == http://* || $1 == https://* ]] ; then echo $1 ; else echo 'First parameter should start with http://, ftp:// or https://' ; fi ; for i in "$@" ; do echo "$i" >> ~/anna/script.log ; done ; else echo 'You nead to provide 4 parameters' >> ~/anna/script.log ; fi
+if [ "$#" -eq "4" ] ; then 
+if [[ $1 == ftp://* || $1 == http://* || $1 == https://* ]] ; then echo $1 ; else echo 'First parameter should start with http://, ftp:// or https://' ; fi  
+for i in "$@" ; do echo "$i" >> ~/anna/script.log ; done 
+else echo 'You nead to provide 4 parameters' >> ~/anna/script.log 
+fi
 
