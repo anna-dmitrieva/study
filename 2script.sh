6a3a3f7e2942e6cda2e620ddf85d3f62da4764b3
anna-dmitrieva <kindly1994@gmail.com>
2017-04-25 21:05:44 +0300

    Update 2script.sh

--- 2script.sh
+++ 2script.sh
@@ -5,7 +5,8 @@ echo $(date -u)": Started" >> ~/anna/script.log
 if [ "$#" -eq "4" ]
 then 
 if [[ $1 == ftp://* || $1 == http://* || $1 == https://* ]] 
-then  wget --user=$2 --password="$3" $1
+then  wget -nv --user=$2 --password="$3" -O ~/anna/prpage $1 
+var=$( command 2>&1 )
 else echo 'First parameter should start with http://, ftp:// or https://' ; fi  
 for i in "$@" ; do echo "$i" >> ~/anna/script.log ; done 
 else echo 'You nead to provide 4 parameters'
