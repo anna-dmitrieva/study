c45827d44ad222ca608e7cb06058ac35389cf080
anna-dmitrieva <kindly1994@gmail.com>
2017-04-26 00:45:07 +0300

    Update 2script.sh

--- 2script.sh
+++ 2script.sh
@@ -8,7 +8,6 @@ then
 if [[ $1 == ftp://* || $1 == http://* || $1 == https://* ]] 
 then  wget -q --user=$2 --password="$3" -O ~/anna/prpage $1 
 N=$[$? + 1] 
-echo "$N"
 sed -n "$N"p ~/anna/errpage
 else echo 'First parameter should start with http://, ftp:// or https://' ; fi  
 for i in "$@" ; do echo "$i" >> ~/anna/script.log ; done 
