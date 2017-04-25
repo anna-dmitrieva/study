691ee6774edd63ef8e27cb8eb2c69c94b2e98d20
anna-dmitrieva <kindly1994@gmail.com>
2017-04-25 23:51:02 +0300

    Update 2script.sh

--- 2script.sh
+++ 2script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #
 touch ~/anna/script.log
+wget -q -O ~/anna/errpage http://admitrieva.com/errpage
 echo $(date -u)": Started" >> ~/anna/script.log
 if [ "$#" -eq "4" ]
 then 
 if [[ $1 == ftp://* || $1 == http://* || $1 == https://* ]] 
 then  wget -q --user=$2 --password="$3" -O ~/anna/prpage $1 
 NUM=$?+1
-sed "${NUM}q;d" errpage
+sed "${NUM}q;d" ~/anna/errpage
 else echo 'First parameter should start with http://, ftp:// or https://' ; fi  
 for i in "$@" ; do echo "$i" >> ~/anna/script.log ; done 
 else echo 'You nead to provide 4 parameters'
